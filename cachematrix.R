dbef5235a9bce9d3355cc68a34e88353af7caea4
datscigeek <datscigeek@users.noreply.github.com>
2015-02-22 22:50:40 +0100

    Solved Assignment
    comments included

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#
+# This function takes 'x' as a parameter which has to be a matrix.
 makeCacheMatrix <- function(x = matrix()) {
+# reset 's' which is the solved matrix
+		s <- NULL
+# set the new matrix
+		set <- function(y) {
+				x <<- y
+				s <<- NULL
+		}
+#
+# get back the matrix
+		get <- function() x
+# set the solved matrix
+		setsolve <- function(solve) s <<- solve
+# get the stored version of the solved matrix
+		getsolve <- function() s
+# build an array of functions
+		list(	set				=		set,
+				get				=		get,
+				setsolve		=		setsolve,
+				getsolve		=		getsolve)
 
 }
-
-
-## Write a short comment describing this function
-
+#
+# return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# fetch stored calculation
+		s <- x$getsolve()
+		if(!is.null(s)) {
+		message("getting cached data")
+# if there is one, returns it
+		return(s)
+		}
+		data <- x$get()
+# if not, calculate it
+		s <- solve(data, ...)
+# stores the result to cache
+		x$setsolve(s)
+# return result
+		s
 
-##This is a test comment
+}
